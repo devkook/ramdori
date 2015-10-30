a9c23abf0b8c5fc918bb86dc4ec1013b13a024c2
min.kyoungkook <min.kyoungkook@cashslide.co.kr>
2015-10-30 12:30:01 +0900

    스플래쉬에 버전을 표기

--- app/src/main/java/com/diginori/ramdori/SplashActivity.java
+++ app/src/main/java/com/diginori/ramdori/SplashActivity.java
@@ -1,20 +1,39 @@
 package com.diginori.ramdori;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Message;
 
 import android.os.Handler;
+import android.widget.TextView;
 
 
 public class SplashActivity extends Activity
 {
+    private TextView mVersionInfo;
+    private Context mContext;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        this.mContext = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        mVersionInfo = (TextView) findViewById(R.id.textViewVersionInfo);
+        String version;
+        try {
+            PackageInfo i = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0);
+            version = i.versionName;
+            mVersionInfo.setText(version);
+        } catch(PackageManager.NameNotFoundException e) { }
+
         initialize();
+
+
     }
 
     private void initialize()
