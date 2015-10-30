6ec68b90dafdf5d4066d9df27bac55867276d85c
min.kyoungkook <min.kyoungkook@cashslide.co.kr>
2015-10-30 15:51:53 +0900

    메일클라이언트 호출 3.0.0

--- app/src/main/java/com/diginori/ramdori/ActMain.java
+++ app/src/main/java/com/diginori/ramdori/ActMain.java
@@ -1,12 +1,15 @@
 package com.diginori.ramdori;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,18 +26,24 @@ public class ActMain extends Activity {
     private Context mContext;
     private ImageView mImgView;
     private EditText mMailTitle;
-    private EditText mMailId;
-    private EditText mMailPasswd;
     private EditText mtoMailId;
     private TextView mIsSuccessTextView;
     private Button mBtnGo;
     private MediaPlayer mp3Player;
+    Realm realm;
+    String yyyymmdd;
+    String rMsg;
+    int year;
+    int mmm;
+    int ddd;
 
     SharedPreferences p_user_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         startActivity(new Intent(this, SplashActivity.class));
+
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -45,8 +54,6 @@ public class ActMain extends Activity {
         mImgView = (ImageView) findViewById(R.id.imageView);
 
         mMailTitle = (EditText) findViewById(R.id.et_mail_title);
-        mMailId = (EditText) findViewById(R.id.et_mail_id);
-        mMailPasswd = (EditText) findViewById(R.id.et_mail_passwd);
         mtoMailId = (EditText) findViewById(R.id.et_to_mail);
 
         mIsSuccessTextView = (TextView) findViewById(R.id.isSuccestTextView);
@@ -58,55 +65,58 @@ public class ActMain extends Activity {
         String new_mail_title = String.format("[헬스장이용신청] %s월%s일 %s", month, day, p_user_info.getString("user_name","임꺽정"));
         mMailTitle.setText(new_mail_title);
 
-        mMailId.setText(p_user_info.getString("mail_id","myid@nbt.com"));
-        mMailPasswd.setText(p_user_info.getString("mail_passwd","1234"));
         mtoMailId.setText(p_user_info.getString("to_mail_id", "to@nbt.com"));
 
         System.out.println(new_mail_title);
+
+        realm = Realm.getInstance(mContext);
 
         mBtnGo = (Button) findViewById(R.id.button);
         mBtnGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String rMsg;
+
 
                 GregorianCalendar today = new GregorianCalendar();
-                int year = today.get(today.YEAR);
-                int month = today.get(today.MONTH) + 1;
-                int day = today.get(today.DAY_OF_MONTH);
+                year = today.get(today.YEAR);
+                mmm = today.get(today.MONTH) + 1;
+                ddd = today.get(today.DAY_OF_MONTH);
 
-                String yyyymmdd = String.format("%s-%02d-%02d", year, month, day);
+                yyyymmdd = String.format("%s-%02d-%02d", year, mmm, ddd);
 
                 try {
                     //DB
-                    Realm realm = Realm.getInstance(mContext);
                     RealmQuery<UserHistory> query = realm.where(UserHistory.class);
                     query.equalTo("yyyymmdd", yyyymmdd);
                     if (query.findAll().size() > 0) {
-                        playSong();
-                        throw new Exception("하루에 한번만!");
+
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                        builder.setTitle("오늘은 메일을 보냈습니다!\n또 보낼까요?");
+                        builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                sendMail();
+                                dialog.dismiss();
+                            }
+                        });
+                        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                playSong();
+                                try {
+                                    throw new Exception("하루에 한번만!");
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                dialog.dismiss();
+                            }
+                        });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    } else {
+                        sendMail();
                     }
 
-                    //
-                    String[] s = mMailTitle.getText().toString().split(" ");
-                    String user_name = s[2];
-                    String new_mail_title = String.format("[헬스장이용신청] %s월%s일 %s", month, day, user_name);
-                    mMailTitle.setText(new_mail_title);
 
-                    savePre(user_name);
-
-                    if(isSendMailFail(new_mail_title)){
-                        playSong();
-                        throw new Exception("메일발송 실패!!!");
-                    }
-
-                    //DB
-                    realm.beginTransaction();
-                    UserHistory uh = realm.createObject(UserHistory.class);
-                    uh.setYyyymmdd(yyyymmdd);
-                    realm.commitTransaction();
-
-                    rMsg = "메일발송 성공!!!";
                 } catch (Exception e) {
                     rMsg = "T T " + e.getMessage();
                 }
@@ -127,29 +137,49 @@ public class ActMain extends Activity {
         });
     }
 
+    private void sendMail() {
+        String[] s = mMailTitle.getText().toString().split(" ");
+        String user_name = s[2];
+        String new_mail_title = String.format("[헬스장이용신청] %s월%s일 %s", mmm, ddd, user_name);
+        mMailTitle.setText(new_mail_title);
+
+        savePre(user_name);
+
+        popupMailClinet(new_mail_title);
+
+        //DB
+        realm.beginTransaction();
+        UserHistory uh = realm.createObject(UserHistory.class);
+        uh.setYyyymmdd(yyyymmdd);
+        realm.commitTransaction();
+        rMsg = "카피카피 룸룸 ~";
+    }
+
+    private void popupMailClinet(String new_mail_title) {
+
+
+        String bodyText = "카피카피 룸룸 ~";
+        String uriText = "mailto:"+mtoMailId.getText().toString() +
+                "?body=" + Uri.encode(bodyText);
+        Uri uri = Uri.parse(uriText);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(uri);
+        intent.putExtra(Intent.EXTRA_SUBJECT, new_mail_title);
+
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            Dialog dialog = new Dialog(this);
+            dialog.setTitle("이메일 클라이언트 호출이상");
+            dialog.show();
+        }
+    }
+
     private void savePre(String user_name) {
         SharedPreferences.Editor e = p_user_info.edit();
         e.putString("user_name", user_name);
-        e.putString("mail_id", mMailId.getText().toString());
-        e.putString("mail_passwd", mMailPasswd.getText().toString());
         e.putString("to_mail_id", mtoMailId.getText().toString());
         e.commit();
-    }
-
-    private boolean isSendMailFail(String new_mail_title) {
-        GMailSender sender = new GMailSender(mMailId.getText().toString(), mMailPasswd.getText().toString()); // SUBSTITUTE HERE
-        try {
-            sender.sendMail(
-                    new_mail_title,   //subject.getText().toString(),
-                    "밀크티만드는개발자@2015",           //body.getText().toString(),
-                    mMailId.getText().toString(),          //from.getText().toString(),
-                    mtoMailId.getText().toString()            //to.getText().toString()
-            );
-            return false;
-        } catch (Exception eee) {
-            Log.e("SendMail", eee.getMessage(), eee);
-            return true;
-        }
     }
 
     private void playSong() {
